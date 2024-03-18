b30ac75acec9aed936f764b77b4c56c0cb1ba0a4
SabirHussain <sabirhussainkhaaki@gmail.com>
2024-03-18 15:44:31 +0500

    Add project files.

---
+++ Twillio/Controllers/SmsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Twilio.Clients;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+using Twillio.ViewModels;
+
+namespace Twillio.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SmsController : ControllerBase
+    {
+        private readonly ITwilioRestClient _client;
+        public SmsController(ITwilioRestClient client)
+        {
+            _client = client;
+        }
+
+        [HttpPost("send-sms")]
+        public IActionResult SendSms(SmsMessage model)
+        {
+            var message = MessageResource.Create(
+                to: new PhoneNumber(model.To),
+                from: new PhoneNumber(model.From),
+                body: model.Message,
+                client: _client); // pass in the custom client
+            return Ok("Success" + message.Sid);
+        }
+    }
+}
---
+++ Twillio/Services/TwilioClient.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Twilio.Clients;
+using Twilio.Http;
+
+namespace Twillio.Services
+{
+    public class Twilio_Client : ITwilioRestClient
+    {
+        private readonly ITwilioRestClient _innerClient;
+        public Twilio_Client(IConfiguration config, System.Net.Http.HttpClient httpClient)
+        {
+            // customize the underlying HttpClient
+            httpClient.DefaultRequestHeaders.Add("X-Custom-Header", "CustomTwilioRestClient-Demo");
+            _innerClient = new TwilioRestClient(
+                config["Twilio:AccountSid"],
+                config["Twilio:AuthToken"],
+                httpClient: new SystemNetHttpClient(httpClient));
+        }
+        public Response Request(Request request) => _innerClient.Request(request);
+        public Task<Response> RequestAsync(Request request) => _innerClient.RequestAsync(request);
+        public string AccountSid => _innerClient.AccountSid;
+        public string Region => _innerClient.Region;
+        public Twilio.Http.HttpClient HttpClient => _innerClient.HttpClient;
+    }
+}
